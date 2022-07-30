6b481b71782819e7dbde60986481210da726994f
HeadNeck <GossUzell@gmail.com>
2022-07-30 13:16:32 +0300

    Bot text messeges moved to constants.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import Bot, Dispatcher, types
 from datetime import datetime
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from constants import API_TOKEN
+from constants import API_TOKEN, BOT_MSGS
 from keyboards import choose_model_kb, restart_kb
 from aiogram.types import ReplyKeyboardRemove
 
@@ -26,17 +26,17 @@ class FSMUser(StatesGroup):
 @dp.message_handler(commands=['start'], state=None)
 async def load_models_menu(message: types.Message):
     await FSMUser.model_menu.set()
-    await message.reply("Choose a model", reply_markup=choose_model_kb)
+    await message.reply(BOT_MSGS["model_menu"], reply_markup=choose_model_kb)
 
 @dp.message_handler(commands=['sr_gan'], state=FSMUser.model_menu)
 async def load_models_menu(message: types.Message, state: FSMContext):
     await FSMUser.sr_gan_wait_photo.set()
-    await message.reply("Send photo please (please choose a photo smaller than 150x150)", reply_markup=ReplyKeyboardRemove())
+    await message.reply(BOT_MSGS["photo_request"], reply_markup=ReplyKeyboardRemove())
 
 @dp.message_handler(commands=['swinir'], state=FSMUser.model_menu)
 async def load_models_menu(message: types.Message, state: FSMContext):
     await FSMUser.swinir_wait_photo.set()
-    await message.reply("Send photo please (please choose a photo smaller than 150x150)", reply_markup=ReplyKeyboardRemove())
+    await message.reply(BOT_MSGS["photo_request"], reply_markup=ReplyKeyboardRemove())
 
 async def work_with_model(model_name, message, state):
     chat_id = message.from_user.id
@@ -45,16 +45,17 @@ async def work_with_model(model_name, message, state):
     width = message.photo[-1]["width"]
     height = message.photo[-1]["height"]
     if ((width>150) or (height>150)):
-        await message.answer("The bot runs on the free heroku server and has very little RAM. Please choose a photo smaller than 150x150")
-        await message.answer("Send photo please (please choose a photo smaller than 150x150)")
+        await message.answer(BOT_MSGS["too_large_photo"])
+        await message.answer(BOT_MSGS["photo_request"])
     else:
         # download and save img
         await (message.photo[-1]
                       .download(os.path.join("lr",
-                                    f"{model_name}/{chat_id}_{send_time}_img.png"),
+                                             model_name,
+                                             f"{chat_id}_{send_time}_img.png"),
                                 make_dirs=False))
-        await message.reply("Please wait until algorythm finish treir work")
-        await message.answer("The bot runs on the free heroku server and has very little RAM. The process may take several minutes.")
+        await message.reply(BOT_MSGS["request_to_wait"])
+        await message.answer(BOT_MSGS["heroku_warning"])
         # send image
         while True:
         	try:
@@ -64,9 +65,9 @@ async def work_with_model(model_name, message, state):
                                     )
         		break
         	except:
-        		print("Wait for image")
         		await asyncio.sleep(1)
-        await message.answer("Here your super photo!", reply_markup=restart_kb)
+        await message.answer(BOT_MSGS["result_congrats"],
+                             reply_markup=restart_kb)
         os.remove(os.path.join("sr",f"{chat_id}_{send_time}_img.png"))
         await state.finish()
 
--- constants.py
+++ constants.py
@@ -15,3 +15,12 @@ MODEL_NAMES = ['sr_gan',
 
 DIRECTORY_LR = 'lr'
 DIRECTORY_SR = 'sr'
+
+BOT_MSGS={
+    "model_menu":"Choose a model",
+    "photo_request":"Send photo please (please choose a photo smaller than 150x150)",
+    "too_large_photo":"The bot runs on the free heroku server and has very little RAM. Please choose a photo smaller than 150x150",
+    "request_to_wait":"Please wait until algorythm finish treir work",
+    "heroku_warning":"The bot runs on the free heroku server and has very little RAM. The process may take several minutes.",
+    "result_congrats":"Here your super photo!",
+}
